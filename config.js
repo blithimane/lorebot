7df5ce29a8547451fe92c0e9a30dadd624af047a
James Tymann <jtymann@the-social-edge.com>
2017-06-26 15:03:57 -0400

    Added logs to debug db

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
 module.exports = {
   "prefix":"!",
   "token":"your_discord_token_here" ,
   "ownerID":"your_discord_username_here",
-  "password":"mysql_password_here",
-  "username":"mysql_username_here",
-  "database":"mysql_database_here"
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
 };
+
+console.log(process.env.DATABASE_URL);
+console.log(JSON.stringify(module.exports));
